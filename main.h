6989e4fa0b2adbfd241a6a860675cd192ceea7dd
Iman Hamza <emyali697@gmail.com>
2023-01-24 17:09:02 +0200

    update main.h wirh print_unsigned

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ int print_string(va_list arg);
 int print_number(int n);
 int (*get_print(char c)) (va_list arg);
 int _printf(const char *format, ...);
+int print_unsigned(va_list args);
 /**
  * struct print - New typedef struct
  * @format: data type
